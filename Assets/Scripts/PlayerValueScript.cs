3cad40f6191db6b6d36bbe24838eccf9ca343e73
uen0_ <sarasa4869@zoho.com>
2022-05-11 23:57:39 +0900

    スプリクトの変更

--- Assets/Scripts/PlayerValueScript.cs
+++ Assets/Scripts/PlayerValueScript.cs
@@ -4,32 +4,62 @@ using UnityEngine;
 
 public class PlayerValueScript : MonoBehaviour,IReceiveDamage
 {
+    /// <summary>
+    /// デバッグモードTrueにすると攻撃を受けない
+    /// </summary>
+    [Header("【デバッグ用】：?をつけると攻撃を受けない"), SerializeField] bool _debugMode = false;
+
+    [Header("HitPoint")]
+    /// <summary>
+    /// HitPoint
+    /// </summary>
     [SerializeField] int _hp = 100;
+    int _maxHp;
+    [SerializeField] GameObject HPController;
+    [SerializeField] HealthController helth;
 
     public bool isGameOver = false;
 
+    public int Hp
+    {
+        set
+        {
+            _hp = Mathf.Clamp(value, 0, _maxHp);
+        }
+        get
+        {
+            return _hp;
+        }
+    }
+
+
     void Start()
     {
-       _hp = Mathf.Clamp(_hp, 0, 100);    
+        _maxHp = HPController.GetComponent<HealthController>()._maxHp;
+        helth = helth.GetComponent<HealthController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //hp処理
+        helth.UpdateSlider(_hp);
+        if (_hp <= 0&& isGameOver == false)
+        {
+            isGameOver = true;
+            Debug.Log("GameOver");
+            GetComponent<Renderer>().material.color = Color.black;
+        }
     }
+
+
     public void ReceiveDamage(int damage)
     {
-        if (isGameOver == false)
+        if (isGameOver == false && _debugMode == false)
         {
             _hp -= damage;
             Debug.Log("add: " + damage + "hp: " + _hp);
-
-            if (_hp <= 0)
-            {
-                isGameOver = true;
-                Debug.Log("GameOver");
-            }
         }
     }
 }
