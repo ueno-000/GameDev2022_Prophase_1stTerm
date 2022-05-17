87f39777fa9a564be889983cd9eaf1246cadca09
uen0_ <sarasa4869@zoho.com>
2022-05-17 14:04:49 +0900

    体力ゲージのローテートスクリプトの追加

---
+++ Assets/Scripts/HPVarRotate.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HPVarRotate : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        //@ƒJƒƒ‰‚Æ“¯‚¶Œü‚«‚Éİ’è
+        transform.rotation = Camera.main.transform.rotation;
+    }
+}
