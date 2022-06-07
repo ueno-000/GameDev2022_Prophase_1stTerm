bd9d0b221595e19c4286cce71fb381cc4d9849af
uen0_ <sarasa4869@zoho.com>
2022-06-07 12:30:07 +0900

    スクリプトの変更

--- Assets/Scripts/Skill/AttackScript.cs
+++ Assets/Scripts/Skill/AttackScript.cs
@@ -5,5 +5,10 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
  
-    [SerializeField] GameObject[] _attackObject;
+    [SerializeField] GameObject[] _prefabObject;
+
+    private void Start()
+    {
+        Instantiate(_prefabObject[0],this.transform.position, Quaternion.identity);
+    }
 }
--- Assets/Scripts/Skill/SkillRotateUnit.cs
+++ Assets/Scripts/Skill/SkillRotateUnit.cs
@@ -4,23 +4,29 @@ using UnityEngine;
  
 public class SkillRotateUnit : MonoBehaviour
 {
-    //　旋回するターゲット
-    [SerializeField] private Transform _player;
+    //数
+    [SerializeField] int _num = 1;
     //　現在の角度
     private float angle;
     //　回転するスピード
-    [SerializeField] private float _rotateSpeed = 180f;
+    [Header("回転するスピード"),SerializeField] private float _rotateSpeed = 180f;
     //　ターゲットからの距離
-    [SerializeField] private Vector3 _distanceFromTarget = new Vector3(0f, 1f, 2f);
+    [Header("ターゲットからの距離"),SerializeField] private Vector3 _distanceFromTarget = new Vector3(0f, 1f, 2f);
 
-    // Update is called once per frame
+     [SerializeField] private GameObject _player;
+
+    void Awake()
+    {
+      // _player = transform.root.gameObject;
+    }
+
     void Update()
     {
-
         //　ユニットの位置 = ターゲットの位置 ＋ ターゲットから見たユニットの角度 ×　ターゲットからの距離
-        transform.position = _player.position + Quaternion.Euler(0f, angle, 0f) * _distanceFromTarget;
+        transform.position = _player.transform.position + Quaternion.Euler(0f, angle, 0f) * _distanceFromTarget;
         //　ユニット自身の角度 = ターゲットから見たユニットの方向の角度を計算しそれをユニットの角度に設定する
-        transform.rotation = Quaternion.LookRotation(transform.position - new Vector3(_player.position.x, transform.position.y, _player.position.z), Vector3.up);
+        transform.rotation = Quaternion.LookRotation
+            (transform.position - new Vector3(_player.transform.position.x, transform.position.y-90, _player.transform.position.z), Vector3.up);
         //　ユニットの角度を変更
         angle += _rotateSpeed * Time.deltaTime;
         //　角度を0～360度の間で繰り返す
