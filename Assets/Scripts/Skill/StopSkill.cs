0938756cc5321e7d59ba43112e16e236d7a418b5
uen0_ <sarasa4869@zoho.com>
2022-06-15 14:47:17 +0900

    時間停止スキルの実装

--- Assets/Scripts/Skill/StopSkill.cs
+++ Assets/Scripts/Skill/StopSkill.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class StopSkill : SkillBase
 {
-    float[] _effectTime = new float[] { 0.5f,1f,1.5f,2f,3f};
+    /// <summary> 効果時間</summary>
+    [SerializeField]  float[] _coolUp = new float[] { 0.5f,1f,1.5f,2f,3f};
+    /// <summary> クールダウン</summary>
+    [SerializeField] float[] _coolDown = new float[] {10,10,8,8,5};
+    /// <summary> 生成位置 </summary>
+    [SerializeField] Vector3[] _pos = new Vector3[6];
 
+    [SerializeField] Transform _player;
+
+    [SerializeField] BoxCollider _collider;
+    float time;
+    GameObject _child;
+    private void Start()
+    {
+        time = 0;
+        _child = transform.GetChild(0).gameObject;
+
+        _child.SetActive(false);
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >=  _coolDown[_skillLevel - 1])
+        {
+            //生成する位置をランダムで決める
+            int num = Random.Range(0,5);
+
+            _child.SetActive(true);
+            _collider.enabled = true;
+            
+            //自身の位置を変える
+            this.gameObject.transform.position = _player.position+_pos[num];
+            
+            time = 0;
+
+            StartCoroutine("Stop");
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.tag == "Enemy")
         {
-            other.GetComponent<IPause>().Pause(_effectTime[_skillLevel]);
+            other.GetComponent<IPause>().Pause(_coolUp[_skillLevel]);
+            
         }
+    }
+
+    IEnumerator Stop()
+    {
+        yield return new WaitForSeconds(_coolUp[_skillLevel-1]);
+        _collider.enabled = false;
+        _child.SetActive(false);
     }
 }
